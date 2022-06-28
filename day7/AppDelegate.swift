05fb10b25c1b22cf6cc4e9b7f3afbe6e5995dc22
Tracy <bestofme2712@gmail.com>
2022-06-28 22:33:17 +0700

    Initial Commit

---
+++ day7/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  day7
+//
+//  Created by Võ Ngọc Vân Anh on 6/28/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ day7/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  day7
+//
+//  Created by Võ Ngọc Vân Anh on 6/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
